5a32f9b3d3dee7d0c6613b526df852b58b0ab5d4
MasumBilla19 <masumiraj0218@gmail.com>
2021-12-22 19:05:26 +0600

    update

---
+++ src/data/daja.js
@@ -0,0 +1,92 @@
+export const ourTeam = [
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+    {
+        cardInfo: {
+            hidden: '',
+            img: '',
+            title: '',
+            subTitle: '',
+            link1: '',
+            link2: ''
+        },
+        img: ''
+    },
+];
+
+
--- src/pages/Hompage/Homepage.jsx
+++ src/pages/Hompage/Homepage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Team from '../../components/OurTeam/OurTeam';
 
 const Homepage = () => {
     return (
         <>
             <Navbar/>
+            <Team />
         </>
     );
 };
