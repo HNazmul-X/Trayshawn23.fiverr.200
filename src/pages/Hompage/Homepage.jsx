9f09a37896b140cc7b0b5426540c8986fe2045f7
H. Nazmul Hassan <Nazmul.w3@gmail.com>
2021-12-22 18:49:43 +0600

    home page header and assets folder added

--- src/pages/Hompage/Homepage.jsx
+++ src/pages/Hompage/Homepage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar';
+import Header from './sections/Header';
 
 const Homepage = () => {
     return (
         <>
-            <Navbar/>
+            <Header/>
         </>
     );
 };
