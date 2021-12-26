83eb45ceacbb3667899054e62a0fa07b2fb8b940
H. Nazmul Hassan <Nazmul.w3@gmail.com>
2021-12-26 11:55:52 +0600

    design section removed

--- src/pages/Hompage/Homepage.jsx
+++ src/pages/Hompage/Homepage.jsx
@@ -1,20 +1,25 @@
-import React from 'react';
-import Header from './sections/Header';
-import MintInformation from './sections/MintInformation';
-import OurTeam from '../../components/OurTeam/OurTeam';
-import Faq from './sections/Faq';
-import DesignSection from './sections/DesignSection';
-import TrailerVideo from './../../components/TrailerVideo/TrailerVideo';
-import Metavers from './../../components/Metavers/Metavers';
-import Roadmap from './../../components/Roadmap/Roadmap';
-import Footer from './../../components/Footer/Footer';
+import React from "react";
+import Header from "./sections/Header";
+import MintInformation from "./sections/MintInformation";
+import OurTeam from "../../components/OurTeam/OurTeam";
+import Faq from "./sections/Faq";
+import DesignSection from "./sections/DesignSection";
+import TrailerVideo from "./../../components/TrailerVideo/TrailerVideo";
+import Metavers from "./../../components/Metavers/Metavers";
+import Roadmap from "./../../components/Roadmap/Roadmap";
+import Footer from "./../../components/Footer/Footer";
 
 const Homepage = () => {
     return (
         <>
-            <Header/>
-            <MintInformation/>
-            <DesignSection/>
+            <Header />
+            <MintInformation />
+
+
+            {/* for visible ths section uncomment the design section component */}
+            {/* <DesignSection/> */}
+
+            
             <Metavers />
             <TrailerVideo />
             <Roadmap />
